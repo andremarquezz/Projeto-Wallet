ee4395576fda1208192857dd3ffb7f2fe7983cc4
Andre Marquez <andre.marqquez@gmail.com>
2022-05-18 14:29:54 -0300

    feat: Adiciona Store e Component Login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,19 @@ import React from 'react';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <div>
+        <form>
+          <input type="email" placeholder="Digite seu email" data-testid="email-input" />
+          <input
+            type="password"
+            placeholder="Digite sua senha"
+            data-testid="password-input"
+          />
+          <button type="submit">Entrar</button>
+        </form>
+      </div>
+    );
   }
 }
 
