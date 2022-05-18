a17a3de041a56d0bc64eafc1c93394fe83eec7fb
Andre Marquez <andre.marqquez@gmail.com>
2022-05-18 17:40:24 -0300

    feat: Adiciona componente Wallet

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Login from './pages/Login';
+import Wallet from './pages/Wallet';
 
 function App() {
   return (
     <Switch>
       <Route exact path="/" component={ Login } />
+      <Route exact path="/carteira" component={ Wallet } />
     </Switch>
   );
 }
