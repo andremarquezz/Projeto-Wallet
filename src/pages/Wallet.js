422098b5c457a9a76c0a1c9fff492905e9415fba
Andre Marquez <andre.marqquez@gmail.com>
2022-05-18 18:43:43 -0300

    feat: Adiciona action thunk

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 import Header from '../components/Header';
+import { getCurrenciesAPI } from '../actions';
 
 class Wallet extends React.Component {
+  componentDidMount() {
+    const { getCurrencies } = this.props;
+    getCurrencies();
+  }
+
   render() {
     return (
       <>
@@ -12,4 +20,11 @@ class Wallet extends React.Component {
   }
 }
 
-export default Wallet;
+const mapDispatchToProps = (dispatch) => ({
+  getCurrencies: () => dispatch(getCurrenciesAPI()),
+});
+
+Wallet.propTypes = {
+  getCurrencies: PropTypes.func.isRequired,
+};
+export default connect(null, mapDispatchToProps)(Wallet);
--- src/reducers/wallet.js
+++ src/reducers/wallet.js
@@ -1,1 +1,17 @@
-// Esse reducer será responsável por tratar o todas as informações relacionadas as despesas
+import { SET_CURRENCIES } from '../actions';
+
+const INITIAL_STATE = {
+  currencies: [],
+};
+
+const walletReducer = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+  case SET_CURRENCIES:
+    console.log('entrei no case');
+    return { ...state, currencies: action.payload };
+  default:
+    return state;
+  }
+};
+
+export default walletReducer;
