64918b75dfaa708ebb6bf63db19ffa47b1f689e8
Andre Marquez <andre.marqquez@gmail.com>
2022-06-02 02:50:52 -0300

    Updates

---
+++ precache-manifest.e0702edf79c30ec5bf4baa330a61366e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67b5852cae536ff518712f40b3e11acb",
+    "url": "/ProjetoWallet/index.html"
+  },
+  {
+    "revision": "39b11e605df7cf237f51",
+    "url": "/ProjetoWallet/static/css/main.0d47a4cb.chunk.css"
+  },
+  {
+    "revision": "ce0570667ee3679efa2d",
+    "url": "/ProjetoWallet/static/js/2.9498f4af.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ProjetoWallet/static/js/2.9498f4af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "39b11e605df7cf237f51",
+    "url": "/ProjetoWallet/static/js/main.d43eab06.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/ProjetoWallet/static/js/main.d43eab06.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "daa488678246c77ce8ac",
+    "url": "/ProjetoWallet/static/js/runtime-main.1b126f62.js"
+  }
+]);
