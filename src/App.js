3c3940aa33c3100c47838ae4044c81d986421033
Andre Marquez <andre.marqquez@gmail.com>
2022-06-01 22:55:28 -0300

    gh pages

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Login from './pages/Login';
 import Wallet from './pages/Wallet';
+import './App.css';
 
 function App() {
   return (
     <Switch>
-      <Route exact path="/" component={ Login } />
-      <Route exact path="/carteira" component={ Wallet } />
+      <Route exact path="/" component={Login} />
+      <Route exact path="/carteira" component={Wallet} />
     </Switch>
   );
 }
